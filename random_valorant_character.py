114344f9cd599e1a0d0280bdb021c6a03cfcb7cd
Treemox <119058960+KingTreemox@users.noreply.github.com>
2022-12-13 00:27:24 +0100

    Add files via upload

---
+++ random_valorant_character.py
@@ -0,0 +1,12 @@
+import random
+
+# List of valorant characters
+characters = ["Sova", "Astra", "Neon", "Raze", "Skye", "Phoenix", "Yoru", "Kay/O", "Breach", "Brimstone", "Jett", "Viper", "Sage", "Cypher", "Omen", "Chamber", "Reyna"]
+
+# Choose a random character from the list
+chosen_character = random.choice(characters)
+
+# Print the chosen character
+print(chosen_character)
+
+# made by KingTreemox aka Treemox
